2742d418f399a730f7bd24332a41e763d7d5f2dc
Mukul_kYadav <ymukul6180@gmail.com>
2022-10-09 18:20:51 +0530

    react repo commited

---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return(
+        <div>
+            <span>Home</span>
+            <span>Contact us</span>
+            <span>About</span>
+        </div>
+    );
+}
